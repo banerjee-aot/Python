46025f169b20e55a0b8d897044363b157234c2c3
BANERJEE <sayan1.banerjee.21@aot.edu.in>
2025-09-04 16:55:49 +0530

    shell py

--- 01_Basics/welcome.py
+++ 01_Basics/welcome.py
@@ -1,3 +1,16 @@
 from hello import welcome
 
 welcome("Welcome To Python")
+
+#Python in shell
+# import os
+# os.getcwd()
+
+# for x in "Sayan":
+#     print(x)
+
+# import sys
+# sys.platform
+
+# For Shell import reload
+# from importlib import reload
